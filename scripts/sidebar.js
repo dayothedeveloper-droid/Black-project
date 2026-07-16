1a96d4f610f5741e33650d37b6208c49db273088
dayoHsg <dayothedeveloper@gmail.com>
2026-07-16 13:10:26 +0100

    Add initial styles and animations for hero section
    - Created animations.js for future animation functions.
    - Added main.css with styles for the hero section, including layout, typography, and button styles.
    - Introduced responsive design for button collection and link container.
    - Added test.html to verify image container styling.

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,23 +3,28 @@ const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".overlay");
 const closeButton = document.querySelector(".close-button");
 
+//  these are called helper functions we create them to make our code cleaner and more scalable 
+
 function openSidebar() {
     sidebar.classList.add("active");
     overlay.classList.add("active");
-    document.body.style.overflow = "hidden"
-}
+    document.body.style.overflow = "hidden";
+};
 
 function closedSidebar() {
      sidebar.classList.remove("active");
      overlay.classList.remove("active");
      document.body.style.overflow = "";
-}
+};
 
 export function sidebarDisplay () {
     hamburgerMenu.addEventListener("click", () => {
         openSidebar();
     });
     closeButton.addEventListener("click", () => {
+        closedSidebar();
+    });
+    overlay.addEventListener("click", () => {
         closedSidebar();
     });
 };
